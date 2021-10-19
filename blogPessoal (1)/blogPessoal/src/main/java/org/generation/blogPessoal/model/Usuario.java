7ad0e585be4dafda92e7457ec51a3e6304c3346d
FeliipeNascimento <felip_nascimento@hotmail.com>
2021-10-19 19:12:20 -0300

    Deploy Concluido

--- blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -2,19 +2,21 @@ package org.generation.blogPessoal.model;
 
 
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
+
 
 @Entity
 @Table(name = "tb_usuario")
-public class Usuario {
+public class Usuario{
 
 		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +26,7 @@ public class Usuario {
 		@Size(min = 2, max = 100, message = "O minimo são 2 caractares e o maximo são 100.")
 		private String nome;
 		
+		@ApiModelProperty(example = "email@email.com.br")
 		@NotNull(message = "O Usuario não pode estar vazio")
 		@Size(min = 5 , max = 100, message = "O minimo são 2 caractares e o maximo são 100.")
 		private String usuario;
